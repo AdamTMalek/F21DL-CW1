c9b5c6f46533279b593ecdf12604c0d3030bc605
Chris Hulme <chris_hulme99@outlook.com>
2020-10-20 08:58:07 +0100

    Main method that crops image, loads cropped image, shuffles instances, prunes non correlating pixels.

--- src/main.py
+++ src/main.py
@@ -5,10 +5,13 @@ from get_top_correlating_pixels import remove_none_corr_pixels
 
 def main():
     crop_images("../data/x_train_gr_smpl.csv", 40)
-    #images = load_image_data_with_ten_one("../data/x_train_gr_smpl_40.csv")
-    # shuffle_dataframe(images)
-    #pruned_images = remove_none_corr_pixels(images, 10)
-    # print(pruned_images)
+    print("Done cropping")
+    images = load_image_data_with_ten_one("../data/x_train_gr_smpl_40.csv")
+    shuffle_dataframe(images)
+    print("Done loading and shuffling")
+    pruned_images = remove_none_corr_pixels(images, 10)
+    print("Done pruning non correlating pixels")
+    print(pruned_images)
 
 
 if __name__ == "__main__":
