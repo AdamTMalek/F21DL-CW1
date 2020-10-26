d3852ecfe78907462e96e5e34c6a882d7c075298
Chris Hulme <chris_hulme99@outlook.com>
2020-10-26 08:30:33 +0000

    Added new naive_bayes function

--- src/naive_bayes.py
+++ src/naive_bayes.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
-from src.get_dataframes import load_image_data_without_y_data, load_all_y_data
+from get_dataframes import load_image_data_without_y_data, load_all_y_data
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn import metrics
 
 
 def process_images():
@@ -11,7 +14,8 @@ def process_images():
     for i in range(len(median_values)):
         # print(median_values[i])
         # print(x_data[i])
-        x_data[i] = x_data[i].apply(lambda x: 0 if x <= median_values[i] else 1)
+        x_data[i] = x_data[i].apply(
+            lambda x: 0 if x <= median_values[i] else 1)
     x_data = x_data.transpose()
     x_data.to_csv("out.csv")
     print(x_data)
@@ -28,6 +32,20 @@ def separate_images_to_csv():
         df.to_csv("../data/individual images/" + str(i) + ".csv")
 
 
+def naive_bayes(x_data, y_data):
+    print(x_data)
+    print(y_data)
+    x_train, x_test, y_train, y_test = train_test_split(
+        x_data, y_data.values.ravel(), test_size=0.4, random_state=1)
+
+    gnb = GaussianNB()
+    gnb.fit(x_train, y_train)
+    y_pred = gnb.predict(x_test)
+    print("Gaussian Naive Bayes model accuracy(in %):",
+          metrics.accuracy_score(y_test, y_pred)*100)
+
+
+"""
 def naive_bayes():
     # Binarise x data and flatten with y
     x_data = process_images()
@@ -48,4 +66,4 @@ def naive_bayes():
     #     # p *= pPixelGivenSign
     #     print(pPixelGivenSign)git
     # print(p)
-
+"""
