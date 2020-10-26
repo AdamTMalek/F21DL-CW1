e32fbcee1695cb3815080fde03ab37a0ef566413
Chris Hulme <chris_hulme99@outlook.com>
2020-10-26 10:58:15 +0000

    Added naive bayes algorithm to main.

--- src/main.py
+++ src/main.py
@@ -1,18 +1,16 @@
 from image_cropper import crop_images
-from get_dataframes import load_image_data_with_ten_one, shuffle_dataframe
+from get_dataframes import load_image_data, load_image_data_with_ten_one, shuffle_dataframe
 from get_top_correlating_pixels import remove_none_corr_pixels
+from naive_bayes import naive_bayes
+import pandas as pd
 
 
 def main():
     crop_images("data/x_train_gr_smpl.csv", 40)
-    print("Done cropping")
-    images = load_image_data_with_ten_one("data/x_train_gr_smpl_40.csv")
-    shuffle_dataframe(images)
-    print(images)
-    print("Done loading and shuffling")
-    pruned_images = remove_none_corr_pixels(images, 10)
-    print("Done pruning non correlating pixels")
-    print(pruned_images)
+    images = load_image_data(
+        "data/x_train_gr_smpl_40.csv", "data/y_train_smpl.csv")
+    shuffled_images = shuffle_dataframe(images)
+    naive_bayes(shuffled_images)
 
 
 if __name__ == "__main__":
