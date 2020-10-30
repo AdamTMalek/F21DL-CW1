36442bf28a0b660119386b4ea73521e8076e3bf7
Chris Hulme <chris_hulme99@outlook.com>
2020-10-30 09:06:37 +0000

    Added more detail to bayes printout.

--- src/naive_bayes.py
+++ src/naive_bayes.py
@@ -2,6 +2,23 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn import metrics
 from dataframe_manipulation import separate_x_and_y
+import pandas as pd
+from classes import classes
+
+
+def print_report(y_test, y_pred):
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', None)
+    pd.set_option('display.max_colwidth', None)
+
+    print("Multinomial Naive Bayes model accuracy(in %):",
+          metrics.accuracy_score(y_test, y_pred)*100)
+    conf_matrix = metrics.confusion_matrix(y_test, y_pred)
+    print(pd.DataFrame(data=conf_matrix, index=classes, columns=classes))
+    classification_rprt = metrics.classification_report(
+        y_test, y_pred, target_names=list(classes))
+    print(classification_rprt)
 
 
 def naive_bayes(images):
@@ -12,5 +29,4 @@ def naive_bayes(images):
     naive_bayes_model = MultinomialNB()
     naive_bayes_model.fit(x_train, y_train)
     y_pred = naive_bayes_model.predict(x_test)
-    print("Multinomial Naive Bayes model accuracy(in %):",
-          metrics.accuracy_score(y_test, y_pred)*100)
+    print_report(y_test, y_pred)
