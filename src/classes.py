74604570b26c5d1f9d0d38cc6422e79c67f0567b
Chris Hulme <chris_hulme99@outlook.com>
2020-10-30 09:07:11 +0000

    Added classes into its own file

---
+++ src/classes.py
@@ -0,0 +1,2 @@
+classes = ["Speed Limit 20", "Speed Limit 30", "Speed Limit 50", "Speed Limit 60", "Speed Limit 70",
+           "Left Turn", "Right Turn", "Beware Pedestrian Crossing", "Beware Children", "Beware Cycle Route Ahead"]
